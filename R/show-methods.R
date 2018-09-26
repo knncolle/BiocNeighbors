3abb97d86d51f6376c8f5478dc40ebd0a33c677c
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-09-26 15:55:09 +0100

    Exported missing show methods.

--- R/show-methods.R
+++ R/show-methods.R
@@ -1,24 +1,29 @@
+#' @export
 #' @importFrom methods show 
 setMethod("show", "BiocNeighborParam", function(object) {
     cat(sprintf("class: %s\n", class(object)))
 })
 
+#' @export
 setMethod("show", "AnnoyParam", function(object) {
     callNextMethod()
     cat(sprintf("ntrees: %i\n", AnnoyParam_ntrees(object)))
     cat(sprintf("directory: %s\n", AnnoyParam_directory(object)))
 })
 
+#' @export
 setMethod("show", "BiocNeighborIndex", function(object) {
     cat(sprintf("class: %s\n", class(object)))
     cat(sprintf("dim: %i %i\n", nrow(object), ncol(object)))
 })
 
+#' @export
 setMethod("show", "KmknnIndex", function(object) {
     callNextMethod()
     cat(sprintf("clusters: %i\n", ncol(KmknnIndex_cluster_centers(object))))
 })
 
+#' @export
 setMethod("show", "AnnoyIndex", function(object) {
     callNextMethod()
     cat(sprintf("path: %s\n", AnnoyIndex_path(object)))
