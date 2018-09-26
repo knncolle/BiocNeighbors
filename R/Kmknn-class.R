090659650962757c7c667579a7ad228a04d4156b
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-09-26 09:41:21 +0100

    Added constructors, validity methods for Kmknn classes.

---
+++ R/Kmknn-class.R
@@ -0,0 +1,35 @@
+#' @export
+#' @importFrom methods new
+KmknnParam <- function() {
+    new("KmknnParam")
+}
+
+#' @export
+#' @importFrom methods new
+KmknnIndex <- function(data, centers, info, order) {
+    new("KmknnIndex", data=data, centers=centers, info=info, order=order)
+}
+
+#' @importFrom S4Vectors setValidity2
+setValidity2("KmknnIndex", function(object) {
+    msg <- character(0)
+
+    data <- KmknnIndex_get_clustered_data(object)
+    centers <- KmknnIndex_get_cluster_centers(object)
+    if (nrow(data)!=nrow(centers)) {
+        msg <- c(msg, "dimensionality is not consistent between 'data' and 'centers") 
+    }
+
+    info <- KmknnIndex_get_cluster_info(object)
+    if (length(info)!=ncol(centers)) {
+        msg <- c(msg, "number of clusters is not consistent between 'centers' and 'info'")
+    }
+
+    order <- KmknnIndex_get_clustered_order(object)
+    if (length(order)!=ncol(data)) {
+        msg <- c(msg, "number of observations is not consistent between 'data' and 'order'")
+    }
+
+    if (length(msg)) return(msg)
+    return(TRUE)
+})
