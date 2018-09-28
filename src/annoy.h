fa482c748268ea98f46127b0937e0f4882accd96
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-09-28 18:50:04 +0100

    Override error printing for Annoy API.

--- src/annoy.h
+++ src/annoy.h
@@ -1,6 +1,9 @@
 #ifndef ANNOY_H
 #define ANNOY_H
 
+// define R's REprintf as the 'local' error print method for Annoy
+#define __ERROR_PRINTER_OVERRIDE__  REprintf
+
 #include "annoylib.h"
 #include "kissrandom.h"
 #include "utils.h"
