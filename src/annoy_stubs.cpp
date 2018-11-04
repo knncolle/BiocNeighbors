8cd089a83484bc1ef8f926a7e4fb030f2822111b
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-11-04 20:40:14 +0000

    Revert "Force all annoy code into a single translation unit."
    This reverts commit 4df4103a22149f6ba1e5921d9a33ea8ee4352839.

--- src/annoy_stubs.cpp
+++
@@ -1,9 +0,0 @@
-#include "init.h"
-#include "annoy.h"
-#include "build_annoy.h"
-#include "find_annoy.h"
-#include "query_annoy.h"
-
-/* Consolidating this into a single object file to avoid multiple definition
- * errors that seem to occur when on Windows 32 only. 
- */
