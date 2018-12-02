46418407581ab52d871edaa3746762b56142a7b5
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-12-02 15:24:20 +0000

    Set up resuable R code for query* functions. Added queryVptree.

--- R/queryKmknn.R
+++ R/queryKmknn.R
@@ -6,63 +6,11 @@ queryKmknn <- function(X, query, k, get.index=TRUE, get.distance=TRUE, BPPARAM=S
 # written by Aaron Lun
 # created 19 June 2018
 {
-    precomputed <- .setup_precluster(X, precomputed, raw.index, ...)
-    k <- .refine_k(k, precomputed, query=TRUE)
-
-    q.out <- .setup_query(query, transposed, subset)
-    query <- q.out$query        
-    job.id <- q.out$index
-    reorder <- q.out$reorder
-
-    # Dividing jobs up for NN finding.
-    jobs <- .assign_jobs(job.id - 1L, BPPARAM)
-    collected <- bpmapply(jobs, FUN=.query_kmknn,
-        MoreArgs=list(data=KmknnIndex_clustered_data(precomputed),
-            centers=KmknnIndex_cluster_centers(precomputed),
-            info=KmknnIndex_cluster_info(precomputed),
-            k=k,
-            query=query,
-            get.index=get.index, 
-            get.distance=get.distance), 
-        BPPARAM=BPPARAM, SIMPLIFY=FALSE)
-
-    # Aggregating results across cores.
-    output <- list()
-    if (get.index) {
-        neighbors <- .combine_matrices(collected, i=1, reorder=reorder)
-        if (!raw.index) {
-            neighbors[] <- KmknnIndex_clustered_order(precomputed)[neighbors]
-        }
-        output$index <- neighbors
-    } 
-    if (get.distance) {
-        output$distance <- .combine_matrices(collected, i=2, reorder=reorder)
-    }
-    return(output)
+    .template_query_exact(X, query, k, get.index=get.index, get.distance=get.distance, BPPARAM=BPPARAM, precomputed=precomputed, 
+        transposed=transposed, subset=subset, raw.index=raw.index, 
+        buildFUN=buildKmknn, searchFUN=.query_kmknn, searchArgsFUN=.find_kmknn_args, orderFUN=KmknnIndex_clustered_order, ...) 
 }
 
 .query_kmknn <- function(jobs, data, centers, info, k, query, get.index, get.distance) {
     .Call(cxx_query_kmknn, jobs, data, centers, info, k, query, get.index, get.distance)
-}
-
-.setup_query <- function(query, transposed, subset) 
-# Convenience wrapper to set up the query.
-{
-    if (!transposed) {
-        query <- t(query)
-    }
-    if (!is.matrix(query)) {
-        query <- as.matrix(query)
-    }
-
-    # Choosing indices.
-    if (!is.null(subset)) {
-        job.id <- .subset_to_index(subset, query, byrow=FALSE)
-        reorder <- order(job.id) # ordering so that queries are adjacent.
-        job.id <- job.id[reorder]
-    } else {
-        job.id <- seq_len(ncol(query))
-        reorder <- NULL
-    }
-    return(list(query=query, index=job.id, reorder=reorder))
 }
---
+++ R/template_query_exact.R
@@ -0,0 +1,59 @@
+#' @export
+#' @importFrom BiocParallel SerialParam bpmapply
+.template_query_exact <- function(X, query, k, get.index=TRUE, get.distance=TRUE, BPPARAM=SerialParam(), precomputed=NULL, transposed=FALSE, subset=NULL, raw.index=FALSE, 
+    buildFUN, searchFUN, searchArgsFUN, orderFUN, ...)
+# Identifies nearest neighbours in 'X' from a query set.
+#
+# written by Aaron Lun
+# created 19 June 2018
+{
+    precomputed <- .setup_precluster(X, precomputed, raw.index, buildFUN=buildFUN, ...)
+    k <- .refine_k(k, precomputed, query=TRUE)
+
+    q.out <- .setup_query(query, transposed, subset)
+    query <- q.out$query        
+    job.id <- q.out$index
+    reorder <- q.out$reorder
+
+    # Dividing jobs up for NN finding.
+    jobs <- .assign_jobs(job.id - 1L, BPPARAM)
+    collected <- bpmapply(jobs, FUN=searchFUN, 
+        MoreArgs=c(searchArgsFUN(precomputed), list(k=k, query=query, get.index=get.index, get.distance=get.distance)), 
+        BPPARAM=BPPARAM, SIMPLIFY=FALSE)
+
+    # Aggregating results across cores.
+    output <- list()
+    if (get.index) {
+        neighbors <- .combine_matrices(collected, i=1, reorder=reorder)
+        if (!raw.index) {
+            neighbors[] <- orderFUN(precomputed)[neighbors]
+        }
+        output$index <- neighbors
+    } 
+    if (get.distance) {
+        output$distance <- .combine_matrices(collected, i=2, reorder=reorder)
+    }
+    return(output)
+}
+
+.setup_query <- function(query, transposed, subset) 
+# Convenience wrapper to set up the query.
+{
+    if (!transposed) {
+        query <- t(query)
+    }
+    if (!is.matrix(query)) {
+        query <- as.matrix(query)
+    }
+
+    # Choosing indices.
+    if (!is.null(subset)) {
+        job.id <- .subset_to_index(subset, query, byrow=FALSE)
+        reorder <- order(job.id) # ordering so that queries are adjacent.
+        job.id <- job.id[reorder]
+    } else {
+        job.id <- seq_len(ncol(query))
+        reorder <- NULL
+    }
+    return(list(query=query, index=job.id, reorder=reorder))
+}
