448593b8755560ff4b1546bc6a9e2f5d6ffefdca
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-12-03 11:29:29 +0000

    Updated KMKNN range finding tests.

--- tests/testthat/test-range-query-kmknn.R
+++ tests/testthat/test-range-query-kmknn.R
@@ -1,5 +1,5 @@
-# Tests queryNeighbors().
-# library(BiocNeighbors); library(testthat); source("test-query-neighbors.R")
+# Tests rangeQueryKmknn().
+# library(BiocNeighbors); library(testthat); source("test-range-query-kmknn.R")
 
 REINFORCE <- function(out) {
     O <- lapply(out$index, order)
@@ -21,7 +21,7 @@ expect_identical_re <- function(left, right) {
 }
 
 set.seed(1001)
-test_that("queryNeighbors() behaves correctly with queries", {
+test_that("rangeQueryKmknn() behaves correctly with queries", {
     ndata <- 1000
     nquery <- 100
 
@@ -29,7 +29,7 @@ test_that("queryNeighbors() behaves correctly with queries", {
         for (d in c(0.1, 0.5, 1)) { 
             X <- matrix(runif(ndata * ndim), nrow=ndata)
             Y <- matrix(runif(nquery * ndim), nrow=nquery)
-            out <- queryNeighbors(X, threshold=d, query=Y)
+            out <- rangeQueryKmknn(X, threshold=d, query=Y)
 
             out.dist <- out.indx <- vector("list", nquery)
             for (j in seq_len(nquery)) {
@@ -45,7 +45,7 @@ test_that("queryNeighbors() behaves correctly with queries", {
 })
 
 set.seed(1002)
-test_that("queryNeighbors() works correctly with subsetting", {
+test_that("rangeQueryKmknn() works correctly with subsetting", {
     nobs <- 1000
 	nquery <- 93
     ndim <- 21
@@ -53,26 +53,26 @@ test_that("queryNeighbors() works correctly with subsetting", {
 
     X <- matrix(runif(nobs * ndim), nrow=nobs)
 	Y <- matrix(runif(nquery * ndim), nrow=nquery)
-    ref <- queryNeighbors(X, Y, threshold=d)
+    ref <- rangeQueryKmknn(X, Y, threshold=d)
     expect_true(length(unique(lengths(ref$index))) > 1L) # some variety; not all, not single.
 
     i <- sample(nquery, 20)
-    sub <- queryNeighbors(X, Y, threshold=d, subset=i)
+    sub <- rangeQueryKmknn(X, Y, threshold=d, subset=i)
     expect_identical_re(sub, lapply(ref, "[", i=i))
 
     i <- rbinom(nquery, 1, 0.5) == 0L
-    sub <- queryNeighbors(X, Y, threshold=d, subset=i)
+    sub <- rangeQueryKmknn(X, Y, threshold=d, subset=i)
     expect_identical_re(sub, lapply(ref, "[", i=i))
 
     rownames(Y) <- paste0("CELL", seq_len(nquery))
     i <- sample(rownames(Y), 50)
-    sub <- queryNeighbors(X, Y, threshold=d, subset=i)
+    sub <- rangeQueryKmknn(X, Y, threshold=d, subset=i)
     m <- match(i, rownames(Y))
     expect_identical_re(sub, lapply(ref, "[", i=m))
 })
 
 set.seed(1003)
-test_that("queryNeighbors() behaves correctly with alternative options", {
+test_that("rangeQueryKmknn() behaves correctly with alternative options", {
     nobs <- 1000
     nquery <- 100
     ndim <- 10
@@ -80,30 +80,30 @@ test_that("queryNeighbors() behaves correctly with alternative options", {
 
     X <- matrix(runif(nobs * ndim), nrow=nobs)
     Y <- matrix(runif(nquery * ndim), nrow=nquery)
-    ref <- queryNeighbors(X, Y, threshold=d)
+    ref <- rangeQueryKmknn(X, Y, threshold=d)
     expect_true(length(unique(lengths(ref$index))) > 1L) # some variety; not all, not single.
     
     # Checking what we extract.
-    out2 <- queryNeighbors(X, Y, threshold=d, get.distance=FALSE)
+    out2 <- rangeQueryKmknn(X, Y, threshold=d, get.distance=FALSE)
     expect_identical(out2$distance, NULL)
     expect_identical(lapply(out2$index, sort), lapply(ref$index, sort))
 
-    out3 <- queryNeighbors(X, Y, threshold=d, get.index=FALSE)
+    out3 <- rangeQueryKmknn(X, Y, threshold=d, get.index=FALSE)
     expect_identical(out3$index, NULL)
     expect_equal(lapply(out3$distance, sort), lapply(ref$distance, sort))
   
     # Checking precomputation.
     pre <- buildKmknn(X)
-    out4 <- queryNeighbors(query=Y, threshold=d, precomputed=pre) # no need for X!
+    out4 <- rangeQueryKmknn(query=Y, threshold=d, precomputed=pre) # no need for X!
     expect_identical_re(out4, ref)
 
     # Checking transposition.
-    out5 <- queryNeighbors(X, threshold=d, query=t(Y), transposed=TRUE)
+    out5 <- rangeQueryKmknn(X, threshold=d, query=t(Y), transposed=TRUE)
     expect_identical_re(out5, ref)
 })
 
 set.seed(100301)
-test_that("queryNeighbors() behaves correctly with parallelization", {
+test_that("rangeQueryKmknn() behaves correctly with parallelization", {
     nobs <- 1001
     nquery <- 101
     ndim <- 11
@@ -111,19 +111,19 @@ test_that("queryNeighbors() behaves correctly with parallelization", {
 
     X <- matrix(runif(nobs * ndim), nrow=nobs)
     Y <- matrix(runif(nquery * ndim), nrow=nquery)
-    ref <- queryNeighbors(X, Y, threshold=d)
+    ref <- rangeQueryKmknn(X, Y, threshold=d)
     expect_true(length(unique(lengths(ref$index))) > 1L) # some variety; not all, not single.
   
     # Trying out different types of parallelization.
-    out1 <- queryNeighbors(X, Y, threshold=d, BPPARAM=MulticoreParam(2))
+    out1 <- rangeQueryKmknn(X, Y, threshold=d, BPPARAM=MulticoreParam(2))
     expect_identical_re(out1, ref)
 
-    out2 <- queryNeighbors(X, Y, threshold=d, BPPARAM=SnowParam(3))
+    out2 <- rangeQueryKmknn(X, Y, threshold=d, BPPARAM=SnowParam(3))
     expect_identical_re(out2, ref)
 })
 
 set.seed(10031)
-test_that("queryNeighbors() raw output behaves correctly", {
+test_that("rangeQueryKmknn() raw output behaves correctly", {
     nobs <- 1001
     nquery <- 101
     ndim <- 11
@@ -132,31 +132,31 @@ test_that("queryNeighbors() raw output behaves correctly", {
   	Y <- matrix(runif(nquery * ndim), nrow=nquery)
  
     pre <- buildKmknn(X)
-    out <- queryNeighbors(query=Y, threshold=d, precomputed=pre, raw.index=TRUE)
-    ref <- queryNeighbors(query=Y, X=t(KmknnIndex_clustered_data(pre)), threshold=d)
+    out <- rangeQueryKmknn(query=Y, threshold=d, precomputed=pre, raw.index=TRUE)
+    ref <- rangeQueryKmknn(query=Y, X=t(KmknnIndex_clustered_data(pre)), threshold=d)
     expect_identical_re(out, ref)
 
     # Behaves with subsetting.
     i <- sample(nquery, 20)
-    out <- queryNeighbors(query=Y, threshold=d, precomputed=pre, raw.index=TRUE, subset=i)
-    ref <- queryNeighbors(query=Y, X=t(KmknnIndex_clustered_data(pre)), threshold=d, subset=i)
+    out <- rangeQueryKmknn(query=Y, threshold=d, precomputed=pre, raw.index=TRUE, subset=i)
+    ref <- rangeQueryKmknn(query=Y, X=t(KmknnIndex_clustered_data(pre)), threshold=d, subset=i)
     expect_identical_re(out, ref)
 
     i <- rbinom(nquery, 1, 0.5) == 0L
-    out <- queryNeighbors(query=Y, threshold=d, precomputed=pre, raw.index=TRUE, subset=i)
-    ref <- queryNeighbors(query=Y, X=t(KmknnIndex_clustered_data(pre)), threshold=d, subset=i)
+    out <- rangeQueryKmknn(query=Y, threshold=d, precomputed=pre, raw.index=TRUE, subset=i)
+    ref <- rangeQueryKmknn(query=Y, X=t(KmknnIndex_clustered_data(pre)), threshold=d, subset=i)
     expect_identical_re(out, ref)
 
     # Adding row names.
     rownames(Y) <- paste0("CELL", seq_len(nquery))
     i <- sample(rownames(Y), 30)
-    out <- queryNeighbors(query=Y, threshold=d, precomputed=pre, raw.index=TRUE, subset=i)
-    ref <- queryNeighbors(query=Y, X=t(KmknnIndex_clustered_data(pre)), threshold=d, subset=i)
+    out <- rangeQueryKmknn(query=Y, threshold=d, precomputed=pre, raw.index=TRUE, subset=i)
+    ref <- rangeQueryKmknn(query=Y, X=t(KmknnIndex_clustered_data(pre)), threshold=d, subset=i)
     expect_identical_re(out, ref)
 })
 
 set.seed(10032)
-test_that("queryNeighbors() behaves with variable distances", {
+test_that("rangeQueryKmknn() behaves with variable distances", {
     nobs <- 1021
     nquery <- 121
     ndim <- 8
@@ -167,28 +167,28 @@ test_that("queryNeighbors() behaves with variable distances", {
     chosen <- sample(length(available), nquery, replace=TRUE)
     d <- available[chosen]
 
-    out <- queryNeighbors(X, Y, threshold=d)
+    out <- rangeQueryKmknn(X, Y, threshold=d)
     for (a in seq_along(available)) {
         current <- chosen==a
         expect_identical_re(lapply(out, "[", i=current), 
-            queryNeighbors(X, Y, threshold=available[a], subset=current))
+            rangeQueryKmknn(X, Y, threshold=available[a], subset=current))
     }
 
     # Handles subsetting.
     scrambled <- sample(nquery, 40)
-    out2 <- queryNeighbors(X, Y, threshold=d[scrambled], subset=scrambled)
+    out2 <- rangeQueryKmknn(X, Y, threshold=d[scrambled], subset=scrambled)
     expect_identical_re(out2, lapply(out, "[", i=scrambled))
 
     scrambled <- rbinom(nquery, 1, 0.5)==1
-    out2 <- queryNeighbors(X, Y, threshold=d[scrambled], subset=scrambled)
+    out2 <- rangeQueryKmknn(X, Y, threshold=d[scrambled], subset=scrambled)
     expect_identical_re(out2, lapply(out, "[", i=scrambled))
 
     # Handles parallelization.
-    expect_identical_re(out, queryNeighbors(X, Y, threshold=d, BPPARAM=MulticoreParam(3)))
+    expect_identical_re(out, rangeQueryKmknn(X, Y, threshold=d, BPPARAM=MulticoreParam(3)))
 })
 
 set.seed(1004)
-test_that("queryNeighbors() behaves correctly with silly inputs", {
+test_that("rangeQueryKmknn() behaves correctly with silly inputs", {
     nobs <- 1000
 	nquery <- 100
     ndim <- 10
@@ -196,36 +196,36 @@ test_that("queryNeighbors() behaves correctly with silly inputs", {
     Y <- matrix(runif(nquery * ndim), nrow=nquery)
     
     # What happens when the threshold is not positive.
-    expect_error(queryNeighbors(X, Y, threshold=0), "positive")
-    expect_error(queryNeighbors(X, Y, threshold=-1), "positive")
+    expect_error(rangeQueryKmknn(X, Y, threshold=0), "positive")
+    expect_error(rangeQueryKmknn(X, Y, threshold=-1), "positive")
 
     # What happens when 'threshold' is not of appropriate length.
-    expect_error(findNeighbors(X, threshold=0:1), "should be equal to")
+    expect_error(rangeQueryKmknn(X, Y, threshold=0:1), "should be equal to")
 
     # What happens when there are no points in the data.
-    out <- queryNeighbors(X[0,,drop=FALSE], Y, threshold=1)
+    out <- rangeQueryKmknn(X[0,,drop=FALSE], Y, threshold=1)
     expect_identical(lengths(out$index), integer(nquery))
     expect_identical(lengths(out$distance), integer(nquery))
 
     # What happens when there are no points in the query.
-    out <- queryNeighbors(X, Y[0,,drop=FALSE], threshold=1)
+    out <- rangeQueryKmknn(X, Y[0,,drop=FALSE], threshold=1)
     expect_identical(out$index, list())
     expect_identical(out$distance, list())
 
     # What happens when there are no dimensions.
-    out <- queryNeighbors(X[,0], Y[,0], threshold=1)
+    out <- rangeQueryKmknn(X[,0], Y[,0], threshold=1)
     expect_identical(unique(out$index), list(seq_len(nobs)))
     expect_identical(unique(out$distance), list(numeric(nobs)))
 
     # What happens when the query is of a different dimension.
     Z <- matrix(runif(nobs * ndim * 2), nrow=nobs)
-    expect_error(queryNeighbors(X, threshold=1, query=Z), "dimensionality")
+    expect_error(rangeQueryKmknn(X, threshold=1, query=Z), "dimensionality")
 
     # What happens when we request raw.index without precomputed.
-    expect_error(queryNeighbors(X, Y, threshold=1, raw.index=TRUE), "not valid")
+    expect_error(rangeQueryKmknn(X, Y, threshold=1, raw.index=TRUE), "not valid")
 
     # What happens when the query is not, strictly a matrix.
     AA <- data.frame(Y)
     colnames(AA) <- NULL
-    expect_identical_re(queryNeighbors(X, Y, threshold=1), queryNeighbors(X, AA, threshold=1))
+    expect_identical_re(rangeQueryKmknn(X, Y, threshold=1), rangeQueryKmknn(X, AA, threshold=1))
 })
