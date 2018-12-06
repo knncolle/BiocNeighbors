15e3b77b842938c39e6a768c1e4d677052860e68
Aaron Lun <alun@wehi.edu.au>
2018-12-06 09:04:23 +0000

    Pass ... arguments to buildVptree in VP tree functions.

--- R/queryVptree.R
+++ R/queryVptree.R
@@ -1,6 +1,6 @@
 #' @export
 #' @importFrom BiocParallel SerialParam bpmapply
-queryVptree <- function(X, query, k, get.index=TRUE, get.distance=TRUE, BPPARAM=SerialParam(), precomputed=NULL, transposed=FALSE, subset=NULL, raw.index=FALSE)
+queryVptree <- function(X, query, k, get.index=TRUE, get.distance=TRUE, BPPARAM=SerialParam(), precomputed=NULL, transposed=FALSE, subset=NULL, raw.index=FALSE, ...)
 # Identifies nearest neighbours in 'X' from a query set.
 #
 # written by Aaron Lun
@@ -8,7 +8,7 @@ queryVptree <- function(X, query, k, get.index=TRUE, get.distance=TRUE, BPPARAM=
 {
     .template_query_exact(X, query, k, get.index=get.index, get.distance=get.distance, BPPARAM=BPPARAM, precomputed=precomputed, 
         transposed=transposed, subset=subset, raw.index=raw.index, 
-        buildFUN=buildVptree, searchFUN=.query_vptree, searchArgsFUN=.find_vptree_args) 
+        buildFUN=buildVptree, searchFUN=.query_vptree, searchArgsFUN=.find_vptree_args, ...)
 }
 
 .query_vptree <- function(jobs, data, nodes, k, query, get.index, get.distance) {
--- R/rangeFindVptree.R
+++ R/rangeFindVptree.R
@@ -1,8 +1,8 @@
 #' @export
 #' @importFrom BiocParallel SerialParam 
-rangeFindVptree <- function(X, threshold, get.index=TRUE, get.distance=TRUE, BPPARAM=SerialParam(), precomputed=NULL, subset=NULL, raw.index=FALSE) {
+rangeFindVptree <- function(X, threshold, get.index=TRUE, get.distance=TRUE, BPPARAM=SerialParam(), precomputed=NULL, subset=NULL, raw.index=FALSE, ...) {
     .template_range_find_exact(X, threshold, get.index=get.index, get.distance=get.distance, BPPARAM=BPPARAM, precomputed=precomputed, subset=subset, raw.index=raw.index,
-        buildFUN=buildVptree, searchFUN=.range_find_vptree, searchArgsFUN=.find_vptree_args) 
+        buildFUN=buildVptree, searchFUN=.range_find_vptree, searchArgsFUN=.find_vptree_args, ...) 
 }
 
 .range_find_vptree <- function(jobs, data, nodes, threshold, get.index, get.distance) {
