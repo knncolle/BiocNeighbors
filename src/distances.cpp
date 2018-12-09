1f2c8e00c616fb40e4a19b53c3b10f46467022d5
Aaron Lun <alun@wehi.edu.au>
2018-12-09 03:31:13 +0000

    Simplified non-AVX Euclidean distance calculator.

--- src/distances.cpp
+++ src/distances.cpp
@@ -5,25 +5,6 @@
 
 double squared_euclidean_dist(const double* x, const double* y, MatDim_t d) {
     double out=0;
-    if (d > 3) {
-        // Manual loop unrolling to get the same behaviour as AVX.
-        double v1=0, v2=0, v3=0, v4=0;
-        for (; d > 3; d-=4) {
-            double tmp1=x[0]-y[0],
-                tmp2=x[1]-y[1],
-                tmp3=x[2]-y[2],
-                tmp4=x[3]-y[3];
-            v1 += tmp1 * tmp1;
-            v2 += tmp2 * tmp2;
-            v3 += tmp3 * tmp3;
-            v4 += tmp4 * tmp4;
-            x+=4;
-            y+=4;
-        }
-
-        out = v1 + v2 + v3 + v4;
-    }
-
     for (; d > 0; --d, ++x, ++y) {
         const double tmp=*x - *y;
         out+=tmp*tmp;
