bd56cf192466630f8a6d481a42ac87f1ed5b5653
vuonga1 <allison.vuong@gmail.com>
2020-05-03 16:29:36 -0700

    refresh BiocNeighborIndex and BiocNeighborParam documentation with roxygen

--- R/BiocNeighborParam-class.R
+++ R/BiocNeighborParam-class.R
@@ -1,3 +1,36 @@
+#' The BiocNeighborParam class
+#'
+#' A virtual class for specifying the type of nearest-neighbor search algorithm and associated parameters.
+#'
+#' @details
+#' The BiocNeighborParam class is a virtual base class on which other parameter objects are built.
+#' There are currently 5 concrete subclasses:
+#' \describe{
+#'     \item{}{\code{\link{ExhaustiveParam}}: exact nearest-neighbor search with the exhaustive (i.e. brute-force) algorithm.}
+#'     \item{}{\code{\link{KmknnParam}}: exact nearest-neighbor search with the KMKNN algorithm.}
+#'     \item{}{\code{\link{VptreeParam}}: exact nearest-neighbor search with the VP tree algorithm.}
+#'     \item{}{\code{\link{AnnoyParam}}: approximate nearest-neighbor search with the Annoy algorithm.}
+#'     \item{}{\code{\link{HnswParam}}: approximate nearest-neighbor search with the HNSW algorithm.}
+#' These objects hold parameters specifying how each algorithm should be run on an arbitrary data set.
+#' See the associated documentation pages for more details.
+#' }
+#'
+#' @seealso
+#' \code{\link{ExhaustiveParam}},
+#' \code{\link{KmknnParam}},
+#' \code{\link{VptreeParam}},
+#' \code{\link{AnnoyParam}},
+#' and \code{\link{HnswParam}} for constructors.
+#'
+#' \code{\link{buildIndex}}, \code{\link{findKNN}} and \code{\link{queryKNN}} for dispatch.
+#' @author Aaron Lun
+#' @docType class
+#' @name BiocNeighborParam
+#' @aliases BiocNeighborParam, BiocNeighborParam-class, bndistance,BiocNeighborParam-method, 
+#'          show,BiocNeighborParam-method
+NULL
+
+#' Display the class of a BiocNeighborParam \code{object}.
 #' @export
 #' @importFrom methods show 
 setMethod("show", "BiocNeighborParam", function(object) {
@@ -5,6 +38,7 @@ setMethod("show", "BiocNeighborParam", function(object) {
     cat(sprintf("distance: %s\n", bndistance(object)))
 })
 
+#' Return a string specifying the distance metric to be used for searching, usually \code{"Euclidean"} or \code{"Manhattan"}.
 #' @export
 setMethod("bndistance", "BiocNeighborParam", function(x) x@distance)
 
