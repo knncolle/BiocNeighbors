a2a2a361374c2720d1e388aa235c3ca277342d56
vuonga1 <allison.vuong@gmail.com>
2020-05-03 14:02:11 -0700

    refresh Kmknn documentation with roxygen

--- R/Kmknn-class.R
+++ R/Kmknn-class.R
@@ -1,7 +1,27 @@
 ##################################
 ###### KmknnParam methods ########
 ##################################
-
+#' The KmknnParam class
+#'
+#' A class to hold parameters for the KMKNN algorithm for exact nearest neighbor identification.
+#'
+#' @param ... Arguments to be passed to \code{\link{kmeans}}.
+#' @param distance A string specifying the distance metric to use.
+#' Defaults to \code{"Euclidean"}.
+#'
+#' @return A KmknnParam object.
+#'
+#' @details
+#' The KmknnParam class holds any parameters associated with running the KMKNN algorithm.
+#' Currently, this relates to tuning of the k-means step - see \code{\link{buildKmknn}} for details.
+#'
+#' @examples
+#' out <- KmknnParam(iter.max=100)
+#' KmknnParam_kmeans_args(out) 
+#'
+#' @seealso 
+#' \code{\link{buildKmknn}}. 
+#' @author Aaron Lun
 #' @export
 #' @importFrom methods new
 KmknnParam <- function(..., distance="Euclidean") {
@@ -32,7 +52,31 @@ setMethod("spill_args", "KmknnParam", function(x) {
 ##################################
 ###### KmknnIndex methods ########
 ##################################
-
+#' The KmknnIndex class
+#'
+#' A class to hold indexing structures for the KMKNN algorithm for exact nearest neighbor identification.
+#'
+#' @param data A numeric matrix with data points in columns and dimensions in rows.
+#' @param centers A numeric matrix with clusters in columns and dimensions in rows.
+#' @param info A list of statistics for each cluster.
+#' @param order An integer vector of length equal to \code{ncol(data)}, specifying the order of observations.
+#' @param NAMES A character vector of sample names or \code{NULL}.
+#' @param distance A string specifying the distance metric to use.
+#'
+#' @return A KmknnIndex object.
+#'
+#' @details
+#' The KmknnIndex class holds the indexing structure required to run the KMKNN algorithm.
+#' Users should never need to call the constructor explicitly, but should generate instances of KmknnIndex classes with \code{\link{buildKmknn}}.
+#'
+#' @examples
+#' out <- KmknnIndex(data, centers, info, order, NAMES=NULL, distance="Euclidean")
+#' str(KmknnIndex_cluster_centers(out))
+#' str(KmknnIndex_cluster_info(out))
+#'
+#' @seealso 
+#' \code{\link{buildKmknn}}. 
+#' @author Aaron Lun
 #' @export
 #' @importFrom methods new
 KmknnIndex <- function(data, centers, info, order, NAMES=NULL, distance="Euclidean") {
@@ -62,7 +106,6 @@ setValidity2("KmknnIndex", function(object) {
     if (length(msg)) return(msg)
     return(TRUE)
 })
-
 
 #' @export
 setMethod("show", "KmknnIndex", function(object) {
