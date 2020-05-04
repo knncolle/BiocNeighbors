402b48e7d87100af21a1d8a85437c3e6c20eb217
vuonga1 <allison.vuong@gmail.com>
2020-05-03 21:17:54 -0700

    refresh buildIndex documentation with roxygen

--- R/buildIndex.R
+++ R/buildIndex.R
@@ -1,3 +1,44 @@
+#' Build a nearest-neighbor index 
+#'
+#' Build indices for nearest-neighbor searching with different algorithms.
+#'
+#' @param X A numeric matrix where rows correspond to data points and columns correspond to variables (i.e., dimensions).
+#' @param ... Further arguments to be passed to individual methods.
+#' This is guaranteed to include \code{transposed}.
+#' @param BNPARAM A \linkS4class{BiocNeighborParam} object specifying the type of index to be constructed.
+#' This defaults to a \linkS4class{KmknnParam} object if no argument is supplied. 
+#'
+#' @details
+#' Supplying a \linkS4class{ExhaustiveParam} object as \code{BNPARAM} will dispatch to \code{\link{buildExhaustive}}.
+#' 
+#' Supplying a \linkS4class{KmknnParam} object as \code{BNPARAM} will dispatch to \code{\link{buildKmknn}}.
+#' 
+#' Supplying a \linkS4class{VptreeParam} object as \code{BNPARAM} will dispatch to \code{\link{buildVptree}}.
+#' 
+#' Supplying an \linkS4class{AnnoyParam} object as \code{BNPARAM} will dispatch to \code{\link{buildAnnoy}}.
+#' 
+#' Supplying an \linkS4class{HnswParam} object as \code{BNPARAM} will dispatch to \code{\link{buildHnsw}}.
+#' 
+#' @return A \linkS4class{BiocNeighborIndex} object containing indexing structures for the specified algorithm.
+#'
+#' @author Aaron Lun
+#'
+#' @seealso
+#' \code{\link{buildExhaustive}},
+#' \code{\link{buildKmknn}},
+#' \code{\link{buildVptree}},
+#' \code{\link{buildAnnoy}}
+#' and \code{\link{buildHnsw}} for specific methods.  
+#'
+#' @examples
+#' Y <- matrix(rnorm(100000), ncol=20)
+#' k.out <- buildIndex(Y)
+#' a.out <- buildIndex(Y, BNPARAM=AnnoyParam())
+#' 
+#' @name buildIndex
+#' @docType methods
+NULL
+
 .BUILDINDEX_GENERATOR <- function(FUN, ARGS=spill_args) {
     function(X, transposed=FALSE, ..., BNPARAM) {    
         do.call(FUN, c(list(X=X, transposed=transposed, ...), ARGS(BNPARAM)))
