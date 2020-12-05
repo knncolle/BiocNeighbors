21d8e395f7a05601ff5a1ca7d75e451fd7d08fe0
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-12-05 11:23:47 -0800

    Switch to using the RcppAnnoy.h header.

--- src/annoy.h
+++ src/annoy.h
@@ -2,28 +2,10 @@
 #define ANNOY_H
 #include "utils.h"
 
-// Stolen from RcppAnnoy's annoy.cpp, to get it to compile on Win32.
-#if defined(__MINGW32__)
-#undef Realloc
-#undef Free
-#endif
-
-// define R's REprintf as the 'local' error print method for Annoy
-#define __ERROR_PRINTER_OVERRIDE__  REprintf
-
 // turn off AVX always, to avoid small inconsistencies in distance calculations.
 #define NO_MANUAL_VECTORIZATION 1
 
-#include "annoylib.h"
-#include "kissrandom.h"
-
-#if defined __RcppAnnoy_0_16_2__
-#ifdef ANNOYLIB_MULTITHREADED_BUILD
-  typedef AnnoyIndexMultiThreadedBuildPolicy AnnoyIndexThreadedBuildPolicy;
-#else
-  typedef AnnoyIndexSingleThreadedBuildPolicy AnnoyIndexThreadedBuildPolicy;
-#endif
-#endif
+#include "RcppAnnoy.h"
 
 template<class Distance>
 class Annoy {
@@ -38,11 +20,7 @@ public:
 
     typedef int32_t Index_t;
     typedef float Data_t;
-#if defined __RcppAnnoy_0_16_2__
-    typedef AnnoyIndex<Index_t, Data_t, Distance, Kiss64Random, AnnoyIndexThreadedBuildPolicy> _index;
-#else
-    typedef AnnoyIndex<Index_t, Data_t, Distance, Kiss64Random> _index;
-#endif
+    typedef AnnoyIndex<Index_t, Data_t, Distance, Kiss64Random, RcppAnnoyIndexThreadPolicy> _index;
     const std::vector<Index_t>& get_neighbors () const;
     const std::vector<Data_t>& get_distances () const;
 private:
