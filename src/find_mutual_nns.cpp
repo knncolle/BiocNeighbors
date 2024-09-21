8bc19ce4b7505ec902d7e10e86c231de5097363d
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-09-21 00:21:13 -0700

    Use int's consistently in findMutualNN's C++ code.
    This avoids unnecessary signedness-comparison warnings with size_t's.

--- src/find_mutual_nns.cpp
+++ src/find_mutual_nns.cpp
@@ -4,15 +4,15 @@
 
 //[[Rcpp::export(rng=false)]]
 Rcpp::List find_mutual_nns (Rcpp::IntegerMatrix left, Rcpp::IntegerMatrix right) {
-    size_t right_obs = right.ncol();
-    size_t right_k = right.nrow();
+    int right_obs = right.ncol();
+    int right_k = right.nrow();
     std::vector<int> sortedR(right.begin(), right.end());
 
     // Sorting the elements on the right.
     auto sIt = sortedR.begin();
-    for (size_t r = 0; r < right_obs; ++r) {
+    for (int r = 0; r < right_obs; ++r) {
         auto sEnd = sIt;
-        for (size_t i = 0; i < right_k; ++i) {
+        for (int i = 0; i < right_k; ++i) {
             --(*sEnd); // make it 0-indexed.
             ++sEnd;
         }
@@ -21,18 +21,17 @@ Rcpp::List find_mutual_nns (Rcpp::IntegerMatrix left, Rcpp::IntegerMatrix right)
     }
 
     std::vector<int> mutualL, mutualR;
-    size_t left_obs = left.ncol();
-    std::vector<size_t> unsearched_offset(right_obs);
+    std::vector<int> unsearched_offset(right_obs);
 
     // Running through the elements on the left, and doing a binary search for
     // the presence of the left neighbor in each of its right neighbor's
     // nearest lists.
-    for (size_t l = 0; l < left_obs ; ++l) {
+    for (int l = 0, left_obs = left.ncol(); l < left_obs; ++l) {
         auto curcol = left.column(l);
 
         for (auto curval0 : curcol) {
             int curval = curval0 - 1; // make it 0-indexed.
-            size_t& already_searched = unsearched_offset[curval];
+            int& already_searched = unsearched_offset[curval];
             if (already_searched == right_k) {
                 continue;
             }
