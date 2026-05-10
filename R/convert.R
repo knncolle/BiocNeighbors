b25f24036b7560bd9aebf9d5bab0402a43154086
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-05-10 16:14:24 +1000

    Generalized conversion functions to work with all NN results.

--- R/convert.R
+++ R/convert.R
@@ -1,55 +1,198 @@
-#' Converts \code{findKNN} output to alternative representations 
+#' Convert neighbor search results to alternative representations 
 #' 
-#' Convert the output of \code{\link{findKNN}} to alternative representations for more convenient downstream use.
+#' Convert the results of various neighbor search functions to alternative representations for more convenient downstream use.
 #' 
-#' @param knn Named list containing the output from \code{\link{findKNN}}.
-#' This list should have the \code{index} and \code{distance} matrices.
+#' @param results 
+#' For \code{findResultsToMatrix} and \code{findResultsToHits}, a named list containing the output from \code{\link{findKNN}}, \code{\link{findNeighbors}} or related functions. 
+#'
+#' For \code{queryResultsToMatrix} and \code{queryResultsToHits}, a named list containing the output from \code{\link{queryKNN}}, \code{\link{queryNeighbors}} or related functions. 
+#'
+#' In both cases, the list should contain an \code{index} entry, and may optionally contain a \code{distance} entry. 
+#' @param num.pts Integer specifying the number of data points in the \code{X} dataset (see the argument of the same name in \code{\link{findKNN}}, \code{\link{queryKNN}} and friends).
+#'
+#' For \code{findResultsToMatrix} and \code{findResultsToHits}, this can be \code{NULL},
+#' in which case the number of data points is assumed to be the same as the number of query points in \code{results}.
+#' Note that users should specify \code{num.pts} if \code{subset=} is used in \code{\link{findKNN}} or related functions.
+#'
+#' For \code{queryResultsToMatrix} and \code{queryResultsToHits}, this is a required argument.
+#' @param index.transposed Boolean indicating whether \code{results$index} is a transposed matrix, i.e., each column corresponds to a query point.
+#' This should be set to \code{TRUE} if \code{get.index="transposed"} in \code{\link{findKNN}} or \code{\link{queryKNN}}. 
+#' Only relevant if \code{results$index} is a matrix.
+#' @param distance.transposed Boolean indicating whether \code{results$distance} is a transposed matrix, i.e., each column corresponds to a query point.
+#' This should be set to \code{TRUE} if \code{get.distance="transposed"} in \code{\link{findKNN}} or \code{\link{queryKNN}}. 
+#' Only relevant if \code{results$distance} is present and a matrix.
+#' @param query.rows Boolean indicating whether query points should be represented as rows in the returned matrix. 
+#' @param query.from Boolean indicating whether query points should be stored as \code{from} in the returned \link[S4Vectors]{Hits} object.
 #' @param repr String specifing the type of sparse matrix, see \code{?\link[Matrix]{sparseMatrix}} for more details.
 #' 
 #' @return
-#' For \code{convertToSparseMatrix}, a floating-point sparse matrix from the \pkg{Matrix} package.
-#' Each row corresponds to an observation and the columns with non-zero entries represent the neighbors.
-#' The value of each non-zero entry contains the distance to that neighbor.
+#' For \code{findResultsToMatrix} and \code{queryResultsToMatrix}, a sparse matrix from the \pkg{Matrix} package is returned.
+#' \itemize{
+#' \item If \code{query.rows=TRUE}, each row corresponds to a query point and the columns with non-zero entries represent the nearest neighbors.
+#' Otherwise, each column corresponds to a query point.
+#' \item If \code{results} contains \code{distance}, the value of each non-zero entry contains the distance to that neighbor.
+#' Otherwise, a pattern matrix is returned indicating whether each entry is a nearest neighbor.
+#' }
 #'
-#' For \code{convertToSelfHits}, a \link[S4Vectors]{SelfHits} object where \code{from} contains the observations and \code{to} contains the identities of the neighbors.
-#' The \code{distance} metadata field contains the distance from each observation to each of its neighbors.
+#' For \code{findResultsToHits} and \code{queryResultsToHits}, a \link[S4Vectors]{Hits} object is returned.
+#' \itemize{
+#' \item If \code{query.from=TRUE}, \code{from} contains the observations and \code{to} contains the identities of the neighbors.
+#' Otherwise, the query points are stored in \code{to}.
+#' \item If \code{results} contains \code{distance}, a \code{distance} field will be present in \code{\link[S4Vectors]{mcols}},
+#' containing the distance from each observation to each of its neighbors.
+#' }
+#' The output will be a \link[S4Vectors]{SelfHits} instance if \code{num.obs=NULL} for \code{findResultsToHits}.
 #' 
 #' @examples
 #' X <- matrix(rnorm(10000), ncol=20)
 #' out <- findKNN(X, k=5)
-#' 
-#' sm <- convertToSparseMatrix(out)
+#' sm <- findResultsToMatrix(out)
 #' str(sm)
-#' 
-#' sh <- convertToSelfHits(out)
+#' sh <- findResultsToHits(out)
 #' sh
+#'
+#' Y <- matrix(rnorm(1000), ncol=20)
+#' out2 <- queryKNN(query=Y, X=X, k=5)
+#' sm2 <- queryResultsToMatrix(out2, num.pts = nrow(X))
+#' str(sm2)
+#' h2 <- queryResultsToHits(out2, num.pts = nrow(X))
+#' h2
 #'
 #' @seealso
 #' The \code{colPairs} slot of the SingleCellExperiment class, from the \pkg{SingleCellExperiment} package.
 #' 
 #' @export
-#' @name convert
-convertToSparseMatrix <- function(knn, repr = "C") {
-    stopifnot(identical(names(knn), c("index", "distance")))
-    n <- nrow(knn$index)
-    Matrix::sparseMatrix(
-        i = rep(seq_len(n), length.out = length(knn$index)), 
-        j = as.vector(knn$index), 
-        x = as.vector(knn$distance), 
-        dims = c(n, n),
-        repr = repr 
+#' @rdname convert
+findResultsToMatrix <- function(results, num.pts = NULL, index.transposed = FALSE, distance.transposed = FALSE, query.rows = TRUE, repr = "C") {
+    .convert_nn_results(
+        results,
+        n.subject = num.pts,
+        index.transposed = index.transposed,
+        distance.transposed = distance.transposed, 
+        as.hits = FALSE,
+        output.transposed = !query.rows,
+        matrix.repr = repr
+    )
+}
+
+.convert_nn_results <- function(results, n.subject, index.transposed, distance.transposed, as.hits, matrix.repr, output.transposed) {
+    is.mat <- is.matrix(results$index)
+    has.dist <- !is.null(results$distance)
+
+    if (is.mat) {
+        if (index.transposed) {
+            n.query <- ncol(results$index)
+            query <- rep(seq_len(n.query), each = nrow(results$index))
+        } else {
+            n.query <- nrow(results$index)
+            query <- rep(seq_len(n.query), ncol(results$index))
+        }
+
+        subject <- as.vector(results$index)
+
+        if (has.dist) {
+            if (distance.transposed != index.transposed) {
+                val <- as.vector(t(results$distance))
+            } else {
+                val <- as.vector(results$distance)
+            }
+        } else {
+            val <- NULL
+        }
+
+    } else {
+        n.query <- length(results$index)
+        query <- rep(seq_along(results$index), lengths(results$index))
+        subject <- unlist(results$index) 
+        if (has.dist) {
+            val <- unlist(results$distance)
+        } else {
+            val <- NULL
+        }
+    }
+
+    if (is.null(n.subject)) {
+        if (as.hits) {
+            if (!output.transposed) {
+                args <- list(from = query, to = subject)
+            } else {
+                args <- list(from = subject, to = query)
+            }
+            args$nnode <- n.query
+            output <- do.call(S4Vectors::SelfHits, args)
+            if (!is.null(val)) {
+                S4Vectors::mcols(output)$distance <- val
+            }
+            return(output)
+        }
+
+        n.subject <- n.query
+    }
+
+    if (!as.hits) {
+        args <- list(repr = matrix.repr)
+        if (!output.transposed) {
+            args <- c(args, list(i = query, j = subject, dims = c(n.query, n.subject)))
+        } else {
+            args <- c(args, list(i = subject, j = query, dims = c(n.subject, n.query)))
+        }
+        if (!is.null(val)) {
+            args$x <- val
+        }
+        return(do.call(Matrix::sparseMatrix, args))
+
+    } else {
+        if (!output.transposed) {
+            args <- list(from = query, to = subject, nLnode = n.query, nRnode = n.subject)
+        } else {
+            args <- list(from = subject, to = query, nLnode = n.subject, nRnode = n.query)
+        }
+        output <- do.call(S4Vectors::Hits, args)
+        if (!is.null(val)) {
+            S4Vectors::mcols(output)$distance <- val
+        }
+        return(output)
+    }
+}
+
+#' @export
+#' @rdname convert
+findResultsToHits <- function(results, num.pts = NULL, index.transposed = FALSE, distance.transposed = FALSE, query.from = TRUE) {
+    .convert_nn_results(
+        results,
+        n.subject = num.pts,
+        index.transposed = index.transposed,
+        distance.transposed = distance.transposed, 
+        as.hits = TRUE,
+        output.transposed = !query.from,
+        matrix.repr = NULL
     )
 }
 
 #' @export
 #' @rdname convert
-convertToSelfHits <- function(knn){
-    stopifnot(identical(names(knn), c("index", "distance")))
-    n <- nrow(knn$index)
-    S4Vectors::SelfHits(
-        from = rep(seq_len(n), length.out = length(knn$index)),
-        to = as.vector(knn$index),
-        distance = as.vector(knn$distance),
-        nnode = n
+queryResultsToMatrix <- function(results, num.pts, index.transposed = FALSE, distance.transposed = FALSE, query.rows = TRUE, repr = "C") {
+    .convert_nn_results(
+        results,
+        n.subject = num.pts,
+        index.transposed = index.transposed,
+        distance.transposed = distance.transposed, 
+        as.hits = FALSE,
+        output.transposed = !query.rows,
+        matrix.repr = repr
+    )
+}
+
+#' @export
+#' @rdname convert
+queryResultsToHits <- function(results, num.pts, index.transposed = FALSE, distance.transposed = FALSE, query.from = TRUE) {
+    .convert_nn_results(
+        results,
+        n.subject = num.pts,
+        index.transposed = index.transposed,
+        distance.transposed = distance.transposed, 
+        as.hits = TRUE,
+        output.transposed = !query.from,
+        matrix.repr = NULL
     )
 }
--- tests/testthat/test-convert.R
+++ tests/testthat/test-convert.R
@@ -2,40 +2,189 @@
 
 set.seed(99999)
 
-test_that("coversion to sparseMatrix works", {
+test_that("findKNN to matrix works correctly", {
     X <- matrix(rnorm(10000), ncol = 20)
     N <- nrow(X)
     out <- findKNN(X, k = 8)
 
-    res <- convertToSparseMatrix(out)
-    expect_s4_class(res, "sparseMatrix")
+    res <- findResultsToMatrix(out)
+    expect_s4_class(res, "dgCMatrix")
     expect_identical(nrow(res), N)
     expect_identical(ncol(res), N)
 
-    expected.first <- numeric(N)
-    expected.first[out$index[1,]] <- out$distance[1,]
-    expect_identical(expected.first, res[1,])
+    for (i in c(1, 100, N)) {
+        expected <- numeric(N)
+        expected [out$index[i,]] <- out$distance[i,]
+        expect_identical(expected, res[i,])
+    }
 
-    expected.last <- numeric(N)
-    expected.last[out$index[N,]] <- out$distance[N,]
-    expect_identical(expected.last, res[N,])
+    # Works with transposed input.
+    {
+        tout <- findKNN(X, k = 8, get.index="transposed")
+        tres <- findResultsToMatrix(tout, index.transposed=TRUE)
+        expect_identical(res, tres)
+
+        tout <- findKNN(X, k = 8, get.distance="transposed")
+        tres <- findResultsToMatrix(tout, distance.transposed=TRUE)
+        expect_identical(res, tres)
+    }
+
+    # Works with transposed output.
+    {
+        tres <- findResultsToMatrix(out, query.rows = FALSE)
+        expect_identical(res, Matrix::t(tres))
+    }
+
+    # Works with without distances.
+    {
+        out2 <- out
+        out2$distance <- NULL
+        lres <- findResultsToMatrix(out2)
+        expect_s4_class(lres, "ngCMatrix")
+        for (i in c(5, 20, 500)) {
+            expect_identical(which(lres[i,]), sort(out$index[i,]))
+        }
+    }
+
+    # Works with variable numbers of neighbors.
+    {
+        vout <- findKNN(X, k = sample(20, nrow(X), replace=TRUE))
+        vres <- findResultsToMatrix(vout)
+
+        for (i in c(2, 10, 50, 200)) {
+            expected <- numeric(N)
+            expected[vout$index[[i]]] <- vout$distance[[i]]
+            expect_identical(expected, vres[i,])
+        }
+    }
+
+    # Works if a subset is specified.
+    {
+        chosen <- rbinom(nrow(X), 1, 0.5) == 1
+        sout <- findKNN(X, k = 8, subset = chosen)
+        sres <- findResultsToMatrix(sout, num.pts = nrow(X))
+        expect_identical(nrow(sres), sum(chosen))
+        expect_identical(ncol(sres), N)
+        expect_identical(sres, res[chosen,,drop=FALSE])
+    }
 })
 
-test_that("coversion to SelfHits works", {
+test_that("findKNN to hits works correctly", {
     X <- matrix(rnorm(10000), ncol = 20)
     N <- nrow(X)
     out <- findKNN(X, k = 8)
 
-    res <- convertToSelfHits(out)
+    res <- findResultsToHits(out)
     expect_s4_class(res, "SelfHits")
     expect_equal(S4Vectors::nLnode(res), S4Vectors::nRnode(res))
     expect_equal(S4Vectors::nLnode(res), nrow(X))
 
-    first <- res[S4Vectors::from(res) == 1]
-    expect_identical(S4Vectors::to(first), out$index[1,])
-    expect_identical(S4Vectors::mcols(first)$distance, out$distance[1,])
+    for (i in c(1, 2, 4, 8, 16, 32)) {
+        expected <- res[S4Vectors::from(res) == i]
+        expect_identical(S4Vectors::to(expected), out$index[i,])
+        expect_identical(S4Vectors::mcols(expected)$distance, out$distance[i,])
+    }
 
-    last <- res[S4Vectors::from(res) == N]
-    expect_identical(S4Vectors::to(last), out$index[N,])
-    expect_identical(S4Vectors::mcols(last)$distance, out$distance[N,])
+    # Works with transposed output.
+    {
+        tres <- findResultsToHits(out, query.from = FALSE)
+        expect_identical(S4Vectors::from(res), S4Vectors::to(tres))
+        expect_identical(S4Vectors::to(res), S4Vectors::from(tres))
+        expect_identical(S4Vectors::mcols(res)$distance, S4Vectors::mcols(tres)$distance)
+    }
+
+    # Works with without distances.
+    {
+        out2 <- out
+        out2$distance <- NULL
+        lres <- findResultsToHits(out2)
+        expect_identical(S4Vectors::from(res), S4Vectors::from(lres))
+        expect_identical(S4Vectors::to(res), S4Vectors::to(lres))
+        expect_null(S4Vectors::mcols(lres)$distance)
+    }
+
+    # Works if a subset is specified.
+    {
+        chosen <- rbinom(nrow(X), 1, 0.5) == 1
+        sout <- findKNN(X, k = 8, subset = chosen)
+        sres <- findResultsToHits(sout, num.pts = nrow(X))
+
+        expect_s4_class(sres, "Hits")
+        expect_identical(S4Vectors::nLnode(sres), sum(chosen)) 
+        expect_identical(S4Vectors::nRnode(sres), nrow(X))
+
+        for (i in round(c(N/2, N/8, N/16))) {
+            expected <- res[S4Vectors::from(res) == i]
+            expect_identical(S4Vectors::to(expected), out$index[i,])
+            expect_identical(S4Vectors::mcols(expected)$distance, out$distance[i,])
+        }
+    }
+})
+
+# Most of the same machinery is reused from the findKNN tests, so we'll just do
+# some cursory checks for the remaining functions.
+
+test_that("findNeighbors conversions work correctly", {
+    X <- matrix(rnorm(8000), ncol = 20)
+    N <- nrow(X)
+    out <- findNeighbors(X, threshold = 4)
+
+    mat <- findResultsToMatrix(out)
+    for (i in c(10, 100, 20, 50)) {
+        expected <- numeric(N)
+        expected[out$index[[i]]] <- out$distance[[i]]
+        expect_identical(expected, mat[i,])
+    }
+
+    hits <- findResultsToHits(out)
+    for (i in c(1, 2, 4, 8, 16, 32)) {
+        expected <- hits[S4Vectors::from(hits) == i]
+        expect_identical(S4Vectors::to(expected), out$index[[i]])
+        expect_identical(S4Vectors::mcols(expected)$distance, out$distance[[i]])
+    }
+})
+
+test_that("queryKNN conversions work correctly", {
+    X <- matrix(rnorm(8000), ncol = 20)
+    Y <- matrix(rnorm(2000), ncol=20)
+    N <- nrow(X)
+    out <- queryKNN(X=X, query=Y, k = 10)
+
+    mat <- queryResultsToMatrix(out, nrow(X))
+    expect_identical(dim(mat), c(nrow(Y), nrow(X)))
+    for (i in c(9, 19, 99)) {
+        expected <- numeric(N)
+        expected[out$index[i,]] <- out$distance[i,]
+        expect_identical(expected, mat[i,])
+    }
+
+    hits <- queryResultsToHits(out, nrow(X))
+    expect_identical(S4Vectors::nLnode(hits), nrow(Y))
+    expect_identical(S4Vectors::nRnode(hits), nrow(X))
+    for (i in c(8, 18, 80)) {
+        expected <- hits[S4Vectors::from(hits) == i]
+        expect_identical(S4Vectors::to(expected), out$index[i,])
+        expect_identical(S4Vectors::mcols(expected)$distance, out$distance[i,])
+    }
+})
+
+test_that("queryNeighbors conversions work correctly", {
+    X <- matrix(rnorm(8000), ncol = 20)
+    N <- nrow(X)
+    Y <- matrix(rnorm(2000), ncol=20)
+    out <- queryNeighbors(X=X, query=Y, threshold = 4)
+
+    mat <- queryResultsToMatrix(out, nrow(X))
+    for (i in c(9, 19, 29, 79)) {
+        expected <- numeric(N)
+        expected[out$index[[i]]] <- out$distance[[i]]
+        expect_identical(expected, mat[i,])
+    }
+
+    hits <- queryResultsToHits(out, nrow(X))
+    for (i in c(10, 20, 40, 80)) {
+        expected <- hits[S4Vectors::from(hits) == i]
+        expect_identical(S4Vectors::to(expected), out$index[[i]])
+        expect_identical(S4Vectors::mcols(expected)$distance, out$distance[[i]])
+    }
 })
